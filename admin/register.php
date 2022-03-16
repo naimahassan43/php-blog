b1365d3684126840806d1ef4e7b30697b6b93864
Naima Hassan <naima.hassan434@gmail.com>
2022-03-16 23:31:05 +0000

    Dashboard splitout

--- admin/register.php
+++ admin/register.php
@@ -92,7 +92,7 @@
                </a>
             </div>
 
-            <a href="login.html" class="text-center">I already have a membership</a>
+            <a href="index.php" class="text-center">I already have a membership</a>
          </div>
          <!-- /.form-box -->
       </div><!-- /.card -->
